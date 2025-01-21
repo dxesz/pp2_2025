0fd43f32d2816c8522166106dcc4b64538237e29
Daulet Yerdos <dauleterdos55@gmail.com>
2025-01-21 19:24:39 +0500

    update

---
+++ Lab1/3_syntax.py
@@ -0,0 +1,21 @@
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+
+
+x = 5
+y = "Hello, World!"
+
+
+
+#This is a comment.
+print("Hello, World!")
