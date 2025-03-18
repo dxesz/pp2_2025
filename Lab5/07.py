a8c5c28f388c65e2f4cae2bc4d1370777644ecce
Daulet Yerdos <dauleterdos55@gmail.com>
2025-03-18 16:33:33 +0500

    update

--- Lab5/07.py
+++ Lab5/07.py
@@ -1,12 +1,4 @@
-'''
-import re
 
-def snake_to_camel(snake_str):
-    words = snake_str.split("_")
-    return "".join(word.capitalize() for word in words)
-
-print(snake_to_camel("hello_world_example")) 
-'''
 import re
 
 snake_to_camel = ("hello_world_example")
